489fcac0555dbb9c377acb3cb533b14a55c9dce1
Saqib Nadeem <saquibnadeem784@gmail.com>
2025-07-18 12:28:43 +0530

    Fix require() in next.config.file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url); // ✅ allows using require in ESM
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
